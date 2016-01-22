9838b49030efc0d03bedcff6894082233061a549
Anshuman Reddy <AVREDDY@mail.bradley.edu>
2016-01-22 12:19:35 -0600

    First commit

---
+++ plot4.R
@@ -0,0 +1,21 @@
+file <- "household_power_consumption.txt"
+data <- read.table(file, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+sub <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+time <- strptime(paste(sub$Date, sub$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+Power <- as.numeric(sub$Global_active_power)
+RPower <- as.numeric(sub$Global_reactive_power)
+volt <- as.numeric(sub$Voltage)
+sub1 <- as.numeric(sub$Sub_metering_1)
+sub2 <- as.numeric(sub$Sub_metering_2)
+sub3 <- as.numeric(sub$Sub_metering_3)
+png("plot4.png", width=480, height=480)
+par(mfrow = c(2, 2)) 
+plot(time, Power, type="l", xlab="", ylab="Global Active Power", cex=0.2)
+plot(time, volt, type="l", xlab="datetime", ylab="Voltage")
+plot(time, sub1, type="l", ylab="Energy Submetering", xlab="")
+lines(time, sub2, type="l", col="red")
+lines(time, sub3, type="l", col="blue")
+legend("topright", c("sub1", "sub2", "sub3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
+plot(time, RPower, type="l", xlab="datetime", ylab="Global_reactive_power")
+dev.off()
+
